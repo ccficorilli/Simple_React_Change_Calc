455638ad5da0bbd29a15563847261f923e21b242
ccficorilli <ccficorilli@gmail.com>
2018-04-05 23:10:03 -0700

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+  <div>
+    <header>Change Calculator</header>
+    <div className='container'>
+      <div className='row'>
+      <div className='cos-sm'>
+      Enter Information
+      </div>
+      <div className='cos-sm'>
+      How Much is due?
+        <input type='number' value='' /> 
+      </div>   
+      <div className='cos-sm'>
+      How much was received?
+        <input type='number' value='' />
+      </div>
+      <div className='cos-sm'><button type='submit' onClick={this.calculate()}>Calulate</button></div>
+      </div>
+    </div>
+    <div>
+      <div>The total change due is $</div>
+      <div>Twenties</div>
+      <div>Tens</div>
+      <div>Fives</div>
+      <div>Ones</div>
+      <div>Quarters</div>
+      <div>Dimes</div>
+      <div>Nickels</div>
+      <div>Pennies</div>
+      
+    </div>
+  </div>
+  }
+}
+
+export default App;
